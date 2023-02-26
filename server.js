39097e1ad90797318181856fe07112531abe8e4a
Anas KBL <kabila.anas1@gmail.com>
2023-02-26 12:15:36 +0100

    add mongodb connection

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 var path = require('path')
+const db_url = "mongodb+srv://0x4ns:Powerpoint7*@firstcluster.ic0a2vm.mongodb.net/MyDB?retryWrites=true&w=majority";
+var MongoClient = require("mongodb").MongoClient;
 
 const PORT = 8089;
 
@@ -9,6 +11,17 @@ app.use(express.static(__dirname + '/public'));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended:true}));
+
+app.post('/saveMovie', (req, res) => {
+  // Extract the data from the request body
+  console.log(req.body);
+});
+
+MongoClient.connect(db_url, function(error, db) {
+    if (error) return funcCallback(error);
+
+    console.log("Connecté à la base de données 'MyDB'");
+});
 
 app.post('/content', (req, res) => {
   res.sendFile('public/movies.html', {root:__dirname});
