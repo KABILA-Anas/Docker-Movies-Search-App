c322e262a92b8e3fca9a96ad4f9a67765571ee8d
Anas KBL <kabila.anas1@gmail.com>
2023-02-26 14:15:30 +0100

    save movies

--- server.js
+++ server.js
@@ -12,16 +12,29 @@ app.use(express.static(__dirname + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended:true}));
 
-app.post('/saveMovie', (req, res) => {
+/*app.post('/saveMovie', (req, res) => {
   // Extract the data from the request body
   console.log(req.body);
-});
-
-MongoClient.connect(db_url, function(error, db) {
-    if (error) return funcCallback(error);
+  MongoClient.connect(db_url, (err, client) => {
+    //if (error) return funcCallback(error);
 
     console.log("Connecté à la base de données 'MyDB'");
+    const db = client.db('MyDB');
+    db.collection('Movies').insertOne(req.body);
+    //client.close();
 });
+});*/
+
+app.post('/saveMovie', async (req, res) => {
+  const client = await MongoClient.connect(db_url);
+  console.log("Connecté à la base de données 'MyDB'");
+  const db = client.db('MyDB');
+  await db.collection('Movies').insertOne(req.body);
+  res.send('Movie saved');
+  client.close();
+});
+
+
 
 app.post('/content', (req, res) => {
   res.sendFile('public/movies.html', {root:__dirname});
