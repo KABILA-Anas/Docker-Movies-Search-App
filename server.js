a69d5523b81a1fe7f7257a07b7d230d0875d9b5e
Anas KBL <kabila.anas1@gmail.com>
2023-03-22 21:05:31 +0000

    add redis container

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const redis = require('redis');
 var path = require('path')
+const bodyParser = require('body-parser');
 
 const db_url = "mongodb://mongo-container:27017/myproject";
 var MongoClient = require("mongodb").MongoClient;
+var redisClient;
+
 
 const PORT = 8089;
 
@@ -11,7 +15,26 @@ const app = express();
 app.use(express.static(__dirname + '/public'));
 
 app.use(express.json());
+app.use(bodyParser.json());
 app.use(express.urlencoded({ extended:true}));
+
+ //() => {
+  const f = async () => {
+    console.log(`hello`);
+      
+    redisClient =  redis.createClient({
+      url: 'redis://myredis:6379'
+    });
+    await redisClient.connect()
+    redisClient.on('error', (err) => {
+      console.log('Error occured while connecting or accessing redis server');
+    });
+  }
+
+  f()
+
+//}
+/**/
 
 
 //insert a movies in the database
@@ -22,6 +45,11 @@ app.post('/saveMovie', async (req, res) => {
   await db.collection('Movies').insertOne(req.body);
   res.send('Movie saved');
   client.close();
+
+  
+  const data = req.body;
+  await redisClient.LPUSH("recentMovies", JSON.stringify(data), redis.print);
+  //redisClient.expire('tvshow', 10);
 });
 
 
