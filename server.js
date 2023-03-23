d2781af59f7226acd1ffe56e1d760d92770dd2df
Anas KBL <kabila.anas1@gmail.com>
2023-03-23 09:56:48 +0000

    add redis container

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require('express');
 const redis = require('redis');
 var path = require('path')
 const bodyParser = require('body-parser');
+const fetch = require("node-fetch");
 
 const db_url = "mongodb://mongo-container:27017/myproject";
+const API_URL = "http://www.omdbapi.com?apikey=b6003d8a";
 var MongoClient = require("mongodb").MongoClient;
 var redisClient;
 
@@ -47,8 +49,29 @@ app.post('/saveMovie', async (req, res) => {
   client.close();
 
   
-  const data = req.body;
-  await redisClient.LPUSH("recentMovies", JSON.stringify(data), redis.print);
+  /*const data = req.body;
+  await redisClient.LPUSH("recentMovies", JSON.stringify(data), redis.print);*/
+  //redisClient.expire('tvshow', 10);
+});
+
+
+//insert a movies in the database
+app.post('/rSaveMovie', async (req, res) => {
+  const film = req.body;
+
+  //fetching all movies with this title
+  const response = await fetch(`${API_URL}&s=${film.Title}`);
+
+  //transforn response to json
+  const data = await response.json();
+
+  data.Search.map(async (movie) => {
+
+  await redisClient.LPUSH(film.Title, JSON.stringify(movie), redis.print);
+  redisClient.expire(film.Title, 10);
+  console.log("visited");
+  
+  });
   //redisClient.expire('tvshow', 10);
 });
 
